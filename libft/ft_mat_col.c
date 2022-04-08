bd56146f367f31e76cf902dafca8f1b8939f017f
Tommaso <tommy.terribili@gmail.com>
2022-04-08 09:10:30 +0200

    fixed libraries

--- libft/ft_mat_col.c
+++ libft/ft_mat_col.c
@@ -3,18 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   ft_mat_col.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tterribi <tterribi@student.42roma.it>      +#+  +:+       +#+        */
+/*   By: tterribi <tterribi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/07 19:40:47 by tterribi          #+#    #+#             */
-/*   Updated: 2022/04/07 19:43:53 by tterribi         ###   ########.fr       */
+/*   Created: 2022/04/08 09:09:44 by tterribi          #+#    #+#             */
+/*   Updated: 2022/04/08 09:10:08 by tterribi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-ft_mat_col(char *str)
+int	ft_mat_col(char *str)
 {
-	int count;
+	int	count;
 
 	count = 0;
 	while (str[count])
